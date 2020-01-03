25bec4f1fa266311e26009c8b7ff8dba04d9c206
Laowalter <walter.d.guo@gmail.com>
2020-01-03 11:25:34 +0800

    finish read and parse voctxt

--- export/export.go
+++ export/export.go
@@ -7,24 +7,55 @@ import (
 	"os"
 )
 
-func main() {
-	file, err := os.Open("/vocabulary.txt")
+const SPLITLINE string = "----split----"
+const VOCABULARYFILE = "../vocabulary.txt"
 
+type Word struct {
+	name    string
+	explain string
+}
+
+func readVoc(voc string) []Word {
+	/*read voc file parse every word and explaination
+	then save it to a Word array and return it.*/
+
+	file, err := os.Open(voc)
 	if err != nil {
 		log.Fatalf("failed opening file: %s", err)
 	}
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
-	var txtlines []string
 
+	var txtlines []string
 	for scanner.Scan() {
 		txtlines = append(txtlines, scanner.Text())
 	}
-
 	file.Close()
 
-	for _, eachline := range txtlines {
-		fmt.Println(eachline)
+	var words []Word
+	var word Word
+	for n := 0; n < len(txtlines); n++ {
+		if txtlines[n] == SPLITLINE {
+			if n != 0 {
+				words = append(words, word)
+			}
+			n++
+			word.name = txtlines[n]
+			continue
+		}
+		word.explain += txtlines[n]
+		word.explain += "\n"
+	}
+	words = append(words, word)
+	return words
+
+}
+
+func main() {
+	var words []Word
+	words = readVoc(VOCABULARYFILE)
+	for _, word := range words {
+		fmt.Printf("WORD NAME: %s\nWORD EXPLAIN:\n %s", word.name, word.explain)
 	}
 }
